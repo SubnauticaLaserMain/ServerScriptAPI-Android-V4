7de36db9fb363e12b7611d2e536ffea305f43769
SubnauticaLaserMain <123651736+SubnauticaLaserMain@users.noreply.github.com>
2024-05-05 20:08:24 +0200

    Update main.lua
    XD

--- main.lua
+++ main.lua
@@ -28,6 +28,7 @@ local Window = Library:AddWindow({
 local MainTab = Window:AddTab('Main', {default = false})
 local GameTab = Window:AddTab('Game', {default = false})
 local SettingsTab = Window:AddTab('Settings', {default = false})
+local GamesTab = Window:AddTab('Games', {default = false})
 
 
 -- Define MainTab - Sections
@@ -55,6 +56,122 @@ GameInfo:AddLabel('PlaceVersion: '..tostring(game.PlaceVersion))
 
 
 
+
+-- Define GamesTab - Section
+if game.PlaceId == 3851622790 then
+	local RemoteEvents = ReplicatedStorage:WaitForChild('RemoteEvents')
+
+
+
+
+
+
+	local function EquipRole(Role, RoleData)
+		local MakeRole = RemoteEvents:WaitForChild('MakeRole')
+		local OutsideRole = RemoteEvents:WaitForChild('OutsideRole')
+
+
+
+		if Role and RoleData then
+			if Role == 'The Swat' or Role == 'The Officer' then
+				local OutsideRole_Data = {
+					[1] = RoleData[1],
+					[2] = RoleData[3]
+				}
+
+				OutsideRole:FireServer(OutsideRole_Data)
+				MakeRole:FireServer(RoleData)
+			else
+				MakeRole:FireServer(RoleData)
+			end
+		end	
+	end
+
+
+
+	local Break_In_2_Lobby = GamesTab:AddSection('Break In 2 - Lobby')
+
+
+
+
+
+
+	-- Break In 2 - Lobby | Features
+	local RoleSelected = function(...) print('Please Select a Role') end
+
+
+
+
+	local Roles = {
+		['Adults'] = {
+			['The Protector'] = {	
+				['Equip'] = function(usingSkin)
+					if usingSkin then
+						local args = {
+							[1] = 'Bat',
+							[2] = false,
+							[3] = usingSkin
+						}
+
+
+						EquipRole('M', args)
+					end
+				end
+			},
+
+			['The Medic'] = {
+				['Equip'] = function(usingSkin)
+					if usingSkin then
+						local args = {
+							[1] = 'MedKit',
+							[2] = false,
+							[3] = usingSkin
+						}
+
+
+						EquipRole('M', args)
+					end
+				end
+			},
+
+			['The Officer'] = {
+				['Equip'] = function(usingSkin)
+					if usingSkin then
+						local args = {
+							[1] = 'Gun',
+							[2] = false,
+							[3] = usingSkin
+						}
+
+
+						EquipRole('The Officer', args)
+					end
+				end
+			},
+
+			['The Swat'] = {
+				['Equip'] = function(usingSkin)
+					if usingSkin then
+						local args = {
+							[1] = 'SwatGun',
+							[2] = false,
+							[3] = usingSkin
+						}
+
+
+						EquipRole('The Swat', args)
+					end
+				end
+			}
+		}
+	}
+end
+
+
+
+
+
+
 -- Define SettingsTab - Sections
 local CustomizeSection = SettingsTab:AddSection('Customize Library', {default = false})
 
@@ -67,3 +184,10 @@ end)
 local SetKeyForWindow = CustomizeSection:AddBind('Window Key', Enum.KeyCode.RightControl, {toggleable = true, default = false, flag = 'Bind_Flag'}, function(keycode)
     Window:SetKey(keycode)
 end)
+
+
+
+
+
+
+-- loadstring(game:HttpGet('https://raw.githubusercontent.com/SubnauticaLaserMain/ServerScriptAPI-Android-V4/main/main.lua', true))()
